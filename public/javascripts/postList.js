5c7601f669b34afbd4aa07a1a7a080b5e6e8a28d
Zhiyi Jin <victoryjzy@gmail.com>
2022-10-24 23:21:46 -0700

    add default posts

--- public/javascripts/postList.js
+++ public/javascripts/postList.js
@@ -1,35 +1,50 @@
+// By Zhiyi Jin
 console.log("call postList.js");
 
 (async () => {
-    const postList = document.querySelector(".post-list");
-    postList.innerHTML = "";
-    for (let i = 0; i < 10; i++) {
-        const cardBox = document.createElement("div");
-        cardBox.className = "card-box col";
-        const mycard = document.createElement("div");
-        mycard.className = "card text-center";
-        const cardBody = document.createElement("div");
-        cardBody.className = "card-body";
-        const title = document.createElement("h5");
-        title.className = "card-title";
-        title.innerHTML = "title";
-        const description = document.createElement("p");
-        description.className = "card-text";
-        description.innerHTML = "Descrption";
-        const anch = document.createElement("a");
-        anch.className = "btn btn-primary";
-        anch.href = "#";
-        anch.innerHTML = "See Details";
-        const time = document.createElement("div");
-        time.className = "card-footer text-muted";
-        time.innerHTML = "OCT 10th";
+    let posts = await fetch("/api/posts", {
+        method: "get",
+    });
 
-        cardBody.appendChild(title);
-        cardBody.appendChild(description);
-        cardBody.appendChild(anch);
-        mycard.appendChild(cardBody);
-        mycard.appendChild(time);
-        cardBox.appendChild(mycard);
-        postList.appendChild(cardBox);
+    let datas = await posts.json();
+    if (datas.length === 0) {
+        let res = await fetch("/api/posts/createDefaultPosts");
+        window.location.replace("/posts");
+    } else {
+        const postList = document.querySelector(".post-list");
+        postList.innerHTML = "";
+        datas.forEach((data) => {
+            const cardBox = document.createElement("div");
+            cardBox.className = "card-box col";
+            const mycard = document.createElement("div");
+            mycard.className = "card text-center";
+            const cardBody = document.createElement("div");
+            cardBody.className = "card-body";
+            const title = document.createElement("h5");
+            title.className = "card-title";
+            title.innerHTML = data.title;
+            const description = document.createElement("p");
+            description.className = "card-text";
+            description.innerHTML = data.description;
+            const anch = document.createElement("a");
+            anch.className = "btn btn-primary";
+            anch.href = "/posts/" + data._id;
+            anch.innerHTML = "See Details";
+
+            let date = new Date(parseInt(data.createdAt));
+            let month = date.toLocaleString("en-us", { month: "short" });
+            let day = date.getDate();
+            const time = document.createElement("div");
+            time.className = "card-footer text-muted";
+            time.innerHTML = month + " " + day + " ";
+
+            cardBody.appendChild(title);
+            cardBody.appendChild(description);
+            cardBody.appendChild(anch);
+            mycard.appendChild(cardBody);
+            mycard.appendChild(time);
+            cardBox.appendChild(mycard);
+            postList.appendChild(cardBox);
+        });
     }
 })();
--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const databaseManager = require("../db/db");
+
+// By Zhiyi Jin
+// Read all posts
+router.get("/", async (req, res) => {
+    let data;
+    let methodChain = {
+        limit: [50],
+        sort: [{ createdAt: -1 }],
+    };
+    try {
+        data = await databaseManager.read("posts", {}, methodChain);
+    } catch (err) {
+        statusCode = 500;
+        res.send(err);
+    }
+    res.send(JSON.stringify(data));
+});
+
+// By Zhiyi Jin
+// create default post
+router.get("/createDefaultPosts", async function (req, res) {
+    try {
+        await databaseManager.createDefaultPosts("posts");
+    } catch (err) {
+        statusCode = 500;
+    }
+    res.status(statusCode).send("");
+});
 
 module.exports = router;
