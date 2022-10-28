eccddaf5bcf5a68cce3c1258bf18af682c370b64
Akhila Sulgante <asulgante39@gmail.com>
2022-10-27 19:06:59 -0700

    landing page changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        node: true,
-    },
-    extends: ["eslint:recommended", "prettier"],
-    overrides: [],
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-    },
-    rules: {
-        indent: ["error", 4],
-        "linebreak-style": ["error", "unix"],
-        quotes: ["error", "double"],
-        semi: ["error", "always"],
-        "no-console": 1,
-    },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    indent: ["error", 4, { SwitchCase: 1 }],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "no-console": 1,
+  },
 };
--- routes/posts.js
+++ routes/posts.js
@@ -5,30 +5,30 @@ const databaseManager = require("../db/db");
 // By Zhiyi Jin
 // Read all posts
 router.get("/", async (req, res) => {
-    let data;
-    let methodChain = {
-        limit: [50],
-        sort: [{ createdAt: -1 }],
-    };
-    try {
-        data = await databaseManager.read("posts", {}, methodChain);
-    } catch (err) {
-        statusCode = 500;
-        res.send(err);
-    }
-    res.send(JSON.stringify(data));
+  let data;
+  let methodChain = {
+    limit: [50],
+    sort: [{ createdAt: -1 }],
+  };
+  try {
+    data = await databaseManager.read("posts", {}, methodChain);
+  } catch (err) {
+    statusCode = 500;
+    res.send(err);
+  }
+  res.send(JSON.stringify(data));
 });
 
 // By Zhiyi Jin
 // create default post
 router.get("/createDefaultPosts", async function (req, res) {
-    let statusCode = 200;
-    try {
-        await databaseManager.createDefaultPosts("posts");
-    } catch (err) {
-        statusCode = 500;
-    }
-    res.status(statusCode).send("");
+  let statusCode = 200;
+  try {
+    await databaseManager.createDefaultPosts("posts");
+  } catch (err) {
+    statusCode = 500;
+  }
+  res.status(statusCode).send("");
 });
 
 module.exports = router;
